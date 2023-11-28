e82ec62a4fa20fb4e5055c54d7b6ac759510a523
kirangagitau <dennosea@gmail.com>
2023-11-28 20:01:11 +0300

    7 edges py

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[1:3]  # first three
+word_last_2 = word[-2]  # -2 for last 2
+middle_word = word[1:-1]  # middle is btn 1st and last
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
