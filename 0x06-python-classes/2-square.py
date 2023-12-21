dbae572dbf660d60733c6352c5ae8ecfaef19943
kirangagitau <dennosea@gmail.com>
2023-12-21 04:21:49 +0300

    square method with size

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Rep a square."""
+
+    def __init__(self, size=0):
+        """Init  new Square.
+        Args:
+            size (int): The size of square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
