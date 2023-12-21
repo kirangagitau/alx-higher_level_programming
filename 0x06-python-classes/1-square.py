fd6edfc5e63c7cbbe0c4df2d453cb7d7b6f4cda5
kirangagitau <dennosea@gmail.com>
2023-12-21 04:19:22 +0300

    square method with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Defines a square."""
+    def __init__(self, size):
+        """Constructor.
+        Args:
+            size: length square.
+        """
+        self.__size = size
