bbd8b264abc90cd080339f717a751754309c4a12
kirangagitau <dennosea@gmail.com>
2024-01-10 17:50:08 +0300

    from json to string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+
 import json
-"""use python3 interpreter for thi code"""
 
 
 def to_json_string(my_obj):
--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -5,4 +5,5 @@ import json
 
 def from_json_string(my_str):
     """Return py  obj rep of a JSON string."""
-    return json.loads(my_str)
+    str = json.loads(my_str)
+    return (str)
