2d033f0326806b74f9e307241eafd1176247e005
kirangagitau <dennosea@gmail.com>
2024-01-17 20:05:35 +0300

    print hexas

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -2,6 +2,6 @@
 '''use python 3'''
 
 
-for no in range(0, 99):
-    print('{} = 0x{:x}'.format(no, no))
+for i in range(0, 99):
+    print('{} = 0x{:x}'.format(i, i))
     ''' :x is format specifier for hexadecimal'''
