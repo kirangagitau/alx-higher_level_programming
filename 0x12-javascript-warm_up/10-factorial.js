4650f9f31b7b227dcb760705e2279ae2e62de29c
kirangagitau <dennose@gmail.com>
2024-03-13 22:11:20 +0300

    factorials

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,11 +2,11 @@
 
 function factorial(n) {
   if (isNaN(n)) {
-    return 1;
+    return (1);
    } else if ( n === 0) {
-       return 1;
+       return (1);
    } else {
-       return n * factorial(n -1);
+       return (n * factorial(n -1));
    }
 }
 
