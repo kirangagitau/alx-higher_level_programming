6ed7d6a4cc47f24963d5d7ca6ec2ceda8a09fda5
kirangagitau <dennose@gmail.com>
2024-03-13 05:14:15 +0300

    solution 7

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 
-const arg2 = process.argv[2];
-const parseNum = parseInt(arg2);
-
-if (isNaN(parseNum)) {
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
   console.log('Not a number');
 } else {
-  console.log(`My number is: ${parseNum}`);
+  console.log('My number:', parseInt(process.argv[2]));
 }
