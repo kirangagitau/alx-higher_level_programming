bc2aa2897ff9473347e3be308bf532a6dd749eaf
kirangagitau <dennose@gmail.com>
2024-03-15 19:04:44 +0300

    object incrementing

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,12 +1,17 @@
 #!/usr/bin/node
+
+//creating an object for incrementing
 const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
+
 myObject.incr = function () {
   this.value++;
 };
+
+// calling obj & incr
 myObject.incr();
 console.log(myObject);
 myObject.incr();
