a7174f46a5ab23dcd195ba14eb0109cdda914130
gitau <dennosea@gmail.com>
2024-03-18 03:40:56 +0300

    ORM

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" This script return all states """
+if __name__ == '__main__':
+    import MySQLdb  # import sql
+    from sys import argv  # take arguments
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states ORDER BY id""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
