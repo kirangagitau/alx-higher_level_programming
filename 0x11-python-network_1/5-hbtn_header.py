34743a9505bde51573cec446d1f39b6226508a46
kirangagitau <dennosea@gmail.com>
2024-07-06 00:18:21 +0300

    header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+try:
+    res = requests.get(sys.argv[1])
+    head = res.headers['X-Request-Id']
+    print(head)
+except BaseException:
+    pass
